8e3f85921736668ea7fe4ff14d9dd36546011806
Erick Castille <ecastille924@gmail.com>
2020-11-21 21:13:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,5 +7,17 @@ class Show < ActiveRecord::Base
   def Show::most_popular_show
     Show.order(rating: :asc).last
   end
+  
+  def Show::lowest_rating 
+    Show.minimum("rating")
+  end
+  
+  def Show::least_popular_show 
+    Show.order(rating: :asc).first
+  end
+  
+  def Show::ratings_sum 
+    Show.sum("rating")
+  end
 end
 
