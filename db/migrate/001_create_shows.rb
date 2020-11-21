995e2f4886ca4188a56b3fcc4f0c9473ff75696c
Erick Castille <ecastille924@gmail.com>
2020-11-21 20:30:16 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,9 @@
 class CreateShows < ActiveRecord[5.2]
   def change 
     create_table :shows do |t|
-      
+      t.string :name 
+      t.string :network 
+      t.string :day 
+      t.integer :rating 
   end
 end
